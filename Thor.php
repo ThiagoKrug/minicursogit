a90142da49dd19cff210e813dc6fb4b47d89833f
luanzelinskicorrea <78267800+luanzelinskicorrea@users.noreply.github.com>
2021-02-06 15:20:12 -0300

    Corrigi um erro escroto

---
+++ Thor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+	</head>
+	<body>
+	<p>Você sabe ler!</p>
+	<?php
+	
+	$a = 7;
+	$i = 0;
+	
+	for($i; $i<=$a;$i++){
+		echo"$i <br>";
+	}
+	?>
+	</body>
+</html>
